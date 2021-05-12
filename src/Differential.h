150a6feb5ff09af710ac89fef21fd16a459198a9
AS-inf <a.inf.wroc@gmail.com>
2021-05-12 17:02:13 +0200

    add example data flow

---
+++ src/Differential.h
@@ -0,0 +1,15 @@
+//
+// Created by xxx on 12.05.2021.
+//
+
+#ifndef ECG_DIFFERENTIAL_H
+#define ECG_DIFFERENTIAL_H
+
+
+class Differential
+{
+
+};
+
+
+#endif //ECG_DIFFERENTIAL_H
---
+++ src/Integral.h
@@ -0,0 +1,8 @@
+//
+// Created by xxx on 12.05.2021.
+//
+
+#ifndef ECG_INTEGRAL_H
+#define ECG_INTEGRAL_H
+
+#endif //ECG_INTEGRAL_H
